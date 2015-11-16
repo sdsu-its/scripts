465a76f82d8ab166e0203d7ffc465395b7da10d3
Tom Paulus <tom@tompaulus.com>
2015-11-16 15:59:02 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Scripts
+========
+
+### `update_fit_tab.sh`
+Updates the publicly accessible folder that hosts the New Tab page for FIT center with the latest version from [GitHub](https://github.com/sdsu-its/fit-new-tab).
+
+### `update_staff_tab.sh`
+Very similar to `update_fit_tab.sh`, however this script pulls the Links page for staff from [this](https://github.com/sdsu-its/staff-new-tab) GitHub Repo.
---
+++ update_staff_tab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Fetch the latest version of a website from github and copy it to a folder.
+###############################################################################
+
+GITHUBURL="https://codeload.github.com/sdsu-its/staff-new-tab/zip/master"
+REPONAME="staff-new-tab"
+WEBPATH="/home/me/its/public_html/Staff-Start/"
+
+mkdir tmp
+cd tmp
+curl $GITHUBURL --output master.zip --silent -k
+unzip -qq master.zip
+
+chmod 0644 *
+cp -rf $REPONAME"-master/." $WEBPATH
+cd ..
+rm -rf tmp
